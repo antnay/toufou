2d600adbdd5d04df94c8afadb3914a82dbfaa252
Anthony <antnay@uw.edu>
2026-01-09 22:22:24 -0800

    deleted dist/

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const title = document.getElementById("title");
-const button = document.getElementById("btn");
-let count = 0;
-button.addEventListener("click", () => {
-    count++;
-    title.textContent = `Clicked ${count} times`;
-});
