83ed2a2143da6cf8347a1035588bbc4dcfbd3a96
Austin Nguyen <austin11@uw.edu>
2026-01-28 20:47:12 -0800

    feat: add event listeners

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import { initState } from "./state";
+import { createInput } from "./ui";
+import { run } from "./gameloop";
+
 async function main() {
     const state = await initState();
-    run(state);
+    const input = createInput();
+    run(state, input);
 }
 
 main();
